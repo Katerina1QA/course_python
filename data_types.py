f6fbb7cc1c80ac35f9fd7c0bfcf93ce7baee70db
Katerina QA <106487463+Katerina1QA@users.noreply.github.com>
2023-01-23 15:48:19 +0000

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,21 @@
+number_a = 2
+print(number_a, type(number_a))
+number_b = 8
+print(number_b, type(number_b))
+number_c = 6
+print(number_c, type(number_c))
+number_d = 9
+print(number_d, type(number_d))
+
+user_name1 = "Katerina"
+print(user_name1, type("user_name1"))
+user_name2 = "Natali"
+print(user_name2, type("user_name2"))
+user_name3 = "Oksana"
+print(user_name3, type("user_name3"))
+variant_a = "New test"
+print(variant_a, type("variant_a"))
+variant_b = "New home"
+print(variant_b, type("variant_b"))
+variant_c = "New device"
+print(variant_c, type("variant_c"))
